07ec23c343599045b3eff0f5667a00bd956e3ee2
Joelma Costa <joelmacscosta@gmail.com>
2026-04-05 20:48:20 -0300

    Add step 2, perform Data Cleaning.

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 """
-Performs basic cleaning on the data and saves the results in W&B
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
 """
 import argparse
 import logging
 import wandb
+import pandas as pd
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -16,38 +17,77 @@ def go(args):
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
+    logger.info("Downloading artifact")
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    logger.info("Reading dataframe")
+    df = pd.read_csv(artifact_local_path)
+
+    logger.info("Cleaning data")
+    # Perform basic cleaning operations
+    df = df[(df['price'] >= args.min_price) & (df['price'] <= args.max_price)]
+
+    logger.info("Saving cleaned data")
+    df.to_csv(args.output_artifact, index=False)
+
+    logger.info("Creating artifact")
+    artifact = wandb.Artifact(
+        name=args.output_artifact,
+        type=args.output_type,
+        description=args.output_description
+    )
+    artifact.add_file(args.output_artifact)
+
+    logger.info("Uploading artifact to W&B") 
+    run.log_artifact(artifact)
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="This step cleans the data")
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
 
     parser.add_argument(
-        "--parameter1", 
-        type=int,
-        help="An int argument to test",
-        required=True
-    )
-
-    parser.add_argument(
-        "--parameter2", 
-        type=float,
-        help="An float argument to test",
-        required=True
-    )
-
-    parser.add_argument(
-        "--parameter3", 
+        "--input_artifact", 
         type=str,
-        help="An str argument to test",
+        help="Input artifact to be cleaned",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,
+        help="Output artifact after cleaning",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,
+        help="Type of the output artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help="Description of the output artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="Minimum price for filtering",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="Maximum price for filtering",
         required=True
     )
 
